f1ca0c475e74626c4f75e2d0b8af08639e229e98
key27 <dkey.ar@gmail.com>
2023-11-29 08:23:18 +0700

    build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,43 +5,39 @@ plugins {
     id ("kotlin-parcelize")
 }
 android {
-    namespace = "com.example.myapplication"
-    compileSdk = 34
-    defaultConfig {
-        applicationId = "com.example.myapplication"
-        minSdk = 25
-        targetSdk = 33
-        versionCode = 1
-        versionName = "1.0"
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-    dataBinding {
-        enable = true
-    }
-    buildFeatures {
-        viewBinding = true
-    }
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+        namespace = "com.example.myapplication"
+        compileSdk = 34
+        defaultConfig {
+            applicationId = "com.example.myapplication"
+            minSdk = 25
+            targetSdk = 25
+            versionCode = 1
+            versionName = "1.0"
+            testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
-    buildToolsVersion = "34.0.0"
-    ndkVersion = "26.1.10909125"
+
+    dataBinding {
+        enable = true    }
+
+    buildFeatures {
+        viewBinding = true        }
+
+        buildTypes {
+            release {
+                isMinifyEnabled = false
+                proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"),
+                    "proguard-rules.pro"                )            }        }
+        compileOptions {
+            sourceCompatibility = JavaVersion.VERSION_1_8
+            targetCompatibility = JavaVersion.VERSION_1_8        }
+        kotlinOptions {
+            jvmTarget = "1.8"        }
+        buildToolsVersion = "34.0.0"
+        ndkVersion = "26.1.10909125"
 }
 dependencies {
     implementation(platform("org.jetbrains.kotlin:kotlin-bom:1.8.0"))
+    implementation("org.jetbrains.kotlin:kotlin-android-extensions:1.8.22")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
